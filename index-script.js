ef8d912a04d7e80f87ef4874c27ead44218e9a6b
Erick <montoya.bonilla.erick@gmail.com>
2025-07-30 16:31:24 -0600

    rutas corregidas en index script

--- index-script.js
+++ index-script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const initbutton = document.getElementById("initButton");
   if (initbutton) {
       initbutton.addEventListener("click", function () {
-      window.location.href = "/game/game.html";
+      window.location.href = "/game.html";
     });
   } else {
     console.error("No se encontró el botón con id 'initButton'");
