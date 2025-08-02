60b913d032dd19db40f61a97370810871a1d80ca
Erick <montoya.bonilla.erick@gmail.com>
2025-08-01 21:02:40 -0600

    Se establece la plantilla para la lógica de game-script

--- game-script.js
+++ game-script.js
@@ -1,17 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {     //Ya se construyeron los elementos HTML.
+document.addEventListener("DOMContentLoaded", function () {     
+    // Esperar a que todos los elementos del DOM estén completamente cargados
+
+    const shootbutton = document.getElementById("shootButton"); 
+    // Obtener referencia al botón con ID 'shootButton'
+
     const worker = new Worker("timerWorker.js");
-    worker.postMessage("start");
+    // Crear un Web Worker para ejecutar tareas en segundo plano sin bloquear la interfaz
+
+    worker.postMessage({type:"start"});
+    // Enviar mensaje al worker para indicarle que inicie su proceso
+
     worker.onmessage = function (e) {
         console.log("Mensaje del Worker:", e.data); 
+        // Escuchar mensajes enviados por el worker y mostrarlos en consola
     };
-    
-    const shootbutton = document.getElementById("shootButton");
-    if (shootbutton ) {
-        shootbutton .addEventListener("click", function () {
-        alert('Aiuda :c')
-        });
-    } else {
-        console.error("No se encontró el botón con id 'shootbutton '");
-    }
+
+    shootbutton .addEventListener("click", function () {
+        // Registrar el evento de clic en el botón (acción por definir)
+    });
+
 });
 
