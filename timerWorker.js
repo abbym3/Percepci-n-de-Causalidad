c86da1218ed5e301a6463f5199a628e405a7e860
Erick <montoya.bonilla.erick@gmail.com>
2025-08-02 21:26:28 -0600

    Timer Work con contador de 10 segundos listo

--- timerWorker.js
+++ timerWorker.js
@@ -1,5 +1,6 @@
 let startTime = performance.now();
 // Guarda el tiempo actual en milisegundos con decimales (Cuando se ejecute new Worker("timerWorker.js"))
+let blocks_100_ms = 0;
 
 function simulate() {
   const now = performance.now();
@@ -12,15 +13,23 @@ function simulate() {
     const blocksPassed = Math.floor(elapsed / 100);
     //Calcula cuantos bloques de completos de 100 ms han pasado
 
-    console.log(`Pasaron ${elapsed.toFixed(2)} ms`);
+    //console.log(`Pasaron ${elapsed.toFixed(2)} ms`);
     // Enviar mensaje al hilo principal
+
+    /*---------------------------------*/
+    blocks_100_ms += blocksPassed;
+    if (blocks_100_ms >= 100) {
+      //onsole.log(`Pasaron 10 s`);
+      blocks_100_ms = 0; // Reiniciar contador
+    }
+    /*--------------------------------*/
 
     startTime += blocksPassed * 100;
     // Avanza el startTime exactamente la cantidad de tiempo ya procesado
   }
 
-  setTimeout(simulate, 1);
-  // Repetir la función cada 1 ms
+  setTimeout(simulate, 4);
+  // Repetir la función cada 4 ms
 }
 
 simulate();
