612c6ed1484f9f2d6338449c984b2bd3ed98262b
Erick <montoya.bonilla.erick@gmail.com>
2025-08-02 19:24:52 -0600

    Worker con bloques de 10 s

--- timerWorker.js
+++ timerWorker.js
@@ -1,5 +1,6 @@
 let startTime = performance.now();
 // Guarda el tiempo actual en milisegundos con decimales (Cuando se ejecute new Worker("timerWorker.js"))
+let blocks_100_ms = null
 
 function simulate() {
   const now = performance.now();
@@ -11,15 +12,21 @@ function simulate() {
   if (elapsed >= 100) {
     // Si han pasado al menos 100 ms...
 
+    blocks_100_ms += 1
     postMessage(`Tiempo: ${Math.floor(elapsed)} ms`);
     // Enviar mensaje al hilo principal
 
     startTime = performance.now();
     // Nuevo punto de referencia que marca el momento en que se imprimió el último mensaje
+
+    if(blocks_100_ms == 100){
+      postMessage(`han pasado 100 bloques de 100 ms`);
+      blocks_100_ms = 0
+    }
   }
 
-  setTimeout(simulate, 5);
-  // Repetir la función cada 5 ms
+  setTimeout(simulate, 1);
+  // Repetir la función cada 1 ms
 }
 
 simulate();
