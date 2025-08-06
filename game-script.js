a50cff4875bce43d39b060d62687531eef8a6197
Erick <montoya.bonilla.erick@gmail.com>
2025-08-05 23:03:00 -0600

    Se agrga nueva función para animacion de disparo

--- game-script.js
+++ game-script.js
@@ -1,26 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {     
     // Esperar a que todos los elementos del DOM estén completamente cargados
 
-    const worker = new Worker("timerWorker.js");
-    // Crear un Web Worker para ejecutar tareas en segundo plano sin bloquear la interfaz
-
+    const worker = new Worker("timerWorker.js"); // Crear un Web Worker para ejecutar tareas en segundo plano sin bloquear la interfaz
     const shootbutton = document.getElementById("shootButton"); 
-    // Obtener referencia al botón con ID 'shootButton'
+    const armaIzquierda = document.getElementById("armaIzquierda");
+    const armaDerecha = document.getElementById("armaDerecha");
+    let number_try_machine = 0; //Número de intentos de la máquina por generar un CEI
+    let shoots = 0; //Número de disparos (clicks al boton central)
+    let shoots_interval_10s = []; //Arreglo que permite guardar el número de disparos cada 10 segundos 
+    let i = 0; //Indice que permite seleccionar la posición 0 a n, donde i guarda la cantidad de disparos en intervalo n de 10s  
+    let permission_calc_p_machine = true; // Flag para habilitar o bloquear el cálculo de probabilidad de la máquina
 
-    let number_try_machine = 0;
-    //Número de intentos de la máquina por generar un CEI
-
-    let shoots = 0;
-    //Número de disparos (clicks al boton central)
-
-    let shoots_interval_10s = [];
-    //Arreglo que permite guardar el número de disparos cada 10 segundos 
-
-    let i = 0;
-    //Indice que permite seleccionar la posición 0 a n, donde i guarda la cantidad de disparos en intervalo n de 10s  
-
-    let permission_calc_p_machine = true;
-     // Flag para habilitar o bloquear el cálculo de probabilidad de la máquina
 
     function adjust_p1_based_on_clicks(shoots_interval_10s_previous) {
         let p1;
@@ -30,20 +20,17 @@ document.addEventListener("DOMContentLoaded", function () {
             p1 = Math.floor((100 / shoots_interval_10s_previous * Math.random()) + 1); //La probabilidad se ajusta al número de clics del intervalo de 10s anterior 
                                                                                        //Un número mas alto de clics aumenta las probabilidades de obtener 1 
             console.log(`Ajuste_P1: ${p1}`)
-
-            // Si la probabilidad ajustada da 1, se procede a calcular si la máquina hace CEI
-            if (p1 === 1) {
-                p_tick_machine()
+            if (p1 === 1) { 
+                p_tick_machine() // Si la probabilidad ajustada da 1, se procede a calcular si la máquina hace CEI
             }
         }
     }
 
     function p_tick_machine(){
-        let p_tck_machine = Math.floor(Math.random() * 18) + 1;
-        //La probailidad de que la máquina haga un intento es de 1/18
+        let p_tck_machine = Math.floor(Math.random() * 18) + 1; // La probailidad de que la máquina haga un intento es de 1/18
         number_try_machine += 1;
         console.log(`Calculo p_machine: ${p_tck_machine}`)
-        if (p_tck_machine === 2) {
+        if (p_tck_machine === 2) { 
             CEI();
         }
     }
@@ -54,23 +41,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function handle100msTick(){
         //console.log('Han pasado 100 ms');
-
-        if(i > 0 && typeof shoots_interval_10s[i - 1] === "number"){ 
+        if(i > 0 && typeof shoots_interval_10s[i - 1] === "number"){  // Para ejecutar adjust_p1_based_on_the_number_of_clicks se debe tener registrado el número de clicks en el primer intervalo de 10s (i>0)
             adjust_p1_based_on_clicks(shoots_interval_10s[i - 1]); 
         }
-        // Una de las condiciones para ejecutar adjust_p1_based_on_the_number_of_clicks es que se tenga registrado el número de clicks en el primer intervalo de 10s (i>0)
+        
     }
 
     function handle10sTick(){
         //console.log('Han pasado 10s');
-
-        shoots_interval_10s[i] = shoots;
-        //Se guarda el número de disparos del intarvalo de 10 segundos en el indice i
+        shoots_interval_10s[i] = shoots; //Se guarda el número de disparos del intarvalo de 10 segundos en el indice i        
         //console.log(`Número de clics en intervalo anterior: ${shoots_interval_10s[i]}`);
+        shoots = 0; //Reiniciamos el contador de disparos 
+        i += 1; //Avanzamos el indice para almacenar el numero de disparos del siguiente intervalo de 10s
         
-        shoots = 0;
-        i += 1;
-        //Reiniciamos el contador de disparos y configuramos el indice
+    }
+    
+    function guns_animation(){
+        armaIzquierda.classList.add("retroceso_izquierda");
+        armaDerecha.classList.add("retroceso_derecha");
+
+        setTimeout(() => {
+        armaIzquierda.classList.remove("retroceso_izquierda");
+        armaDerecha.classList.remove("retroceso_derecha");
+        }, 100); // Duración del retroceso
     }
 
     worker.onmessage = function (e) {
@@ -87,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     shootbutton.addEventListener("click", function () {
         shoots += 1;
-        // Registrar el evento de clic en el botón 
+        guns_animation();
     });
 
 });
