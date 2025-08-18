8ee7b301b4c3596b6b0f308fe302e0ca52acccde
Erick <montoya.bonilla.erick@gmail.com>
2025-08-17 18:36:19 -0600

    Se regresa setTimeout al Worker (en lugar de RAF)

--- timerWorker.js
+++ timerWorker.js
@@ -1,9 +1,9 @@
 
-let startTime = 0//performance.now() - 60; // Guarda el tiempo actual en milisegundos con decimales (Cuando se ejecute new Worker("timerWorker.js")) - un delay de arranque 
-let blockCounter = 0; // Contador de bloques de 50 ms (útil para contar los 100 ms y los 10 s)
+let startTime = performance.now(); //- 60; // Guarda el tiempo actual en milisegundos con decimales (Cuando se ejecute new Worker("timerWorker.js")) - un delay de arranque 
+let blockCounter = 0; // Contador de bloques de 20 ms (útil para contar los 100 ms y los 10 s)
 let running = true; // Control de pausa
 let pauseTime = null; // Almacena el momento en que se pausó
-let accumulated_time_ms = 0; // Tiempo total acumulado en bloques de 50 ms
+let accumulated_time_ms = 0; // Tiempo total acumulado en bloques de 20 ms
 let offset_calibrado = 0;
 let offset_aplicado = false;
 
@@ -11,8 +11,10 @@ function simulate() {
 
   //console.log("tick", performance.now());
 
-  if (!running) return; // Si no se esta ejecutando, no continuar
-
+  if (!running) {
+    setTimeout(simulate, 4); //El loop se mantiene activo sin calculos
+    return;
+  }
   const now = performance.now(); // Obtener el tiempo actual
   const elapsed = now - startTime; // Calcular cuánto tiempo ha pasado desde el último starTime
 
@@ -33,7 +35,7 @@ function simulate() {
     }
     startTime += blocksPassed * 20; // Avanza startTime la cantidad de tiempo procesado
   }
-  requestAnimationFrame(simulate); // Repetir la función cada 4 ms
+  setTimeout(simulate, 4); // Repetir la función cada 4 ms
 }
 
 simulate(); // Iniciar la simulación
@@ -53,7 +55,6 @@ onmessage = function (e) {
         pauseTime = null;
       }
       running = true;
-      simulate();
       break;
 
     case 'get_time':
@@ -74,6 +75,5 @@ onmessage = function (e) {
       offset_calibrado = e.data.value;
       console.log("Offset recibido en Worker:", offset_calibrado.toFixed(2), "ms");
       break;
-
   }
 };
